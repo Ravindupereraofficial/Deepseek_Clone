4abaccc9a601d645425ad61bd8270df4ab5d3f2a
Ravindu Perera <ravindurandika2004@gmail.com>
2025-08-21 19:30:16 +0530

    feat: add state management for expand, messages, and loading in Home component

--- deepseek/app/page.js
+++ deepseek/app/page.js
@@ -1,6 +1,11 @@
-import Image from "next/image";
+
+import { useState } from "react";
+'use client';
 
 export default function Home() {
+  const [expand,setExpand] = useState(false)
+  const [messages,setMessages] = useState([])
+  const [isLoading,setIsLoading] = useState(false)
   return (
    <div></div>
   );
