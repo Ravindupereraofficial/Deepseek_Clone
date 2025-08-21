e5241811b131e5a108c72ea31fdb1e2427021aa7
Ravindu Perera <ravindurandika2004@gmail.com>
2025-08-21 19:43:08 +0530

    refactor: update global styles and restructure Home component layout

--- deepseek/app/page.js
+++ deepseek/app/page.js
@@ -1,12 +1,25 @@
 
-import { useState } from "react";
 'use client';
+import { assets } from "@/assets/assets";
+import { useState } from "react";
+import Image from "next/image";
+
 
 export default function Home() {
   const [expand,setExpand] = useState(false)
   const [messages,setMessages] = useState([])
   const [isLoading,setIsLoading] = useState(false)
   return (
-   <div></div>
+   <div>
+    <div className="flex h-screen">
+      {}
+    <div>
+      <div>
+        <Image className="rotate-180" src={assets.menu_icon} alt=""/>
+        <Image className="opacity-70" src={assets.chat_icon} alt=""/>
+      </div>
+    </div>
+    </div>
+   </div>
   );
 }
